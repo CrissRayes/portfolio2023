9b234f393c68f0916120daee7fe9a0c9f8f06f0f
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-11-18 12:22:31 -0300

    Refactor image loading in Synaptech, Urbanus, and Vernux components

--- src/components/Vernux.jsx
+++ src/components/Vernux.jsx
@@ -1,44 +1,17 @@
 import { FaGlobe } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
-import useImageLoader from '../hooks/useImageLoader';
-import { SkeletonLoader } from './SkeletonLoader';
 
 export const Vernux = () => {
-  const imageUrls = ['/img/vernux-web.png'];
-  const imagesLoaded = useImageLoader(imageUrls);
   return (
     <div className='project-detail'>
-      {imageUrls.map((url, index) => (
-        <div
-          key={index}
-          className='image-container'
-        >
-          {!imagesLoaded[index] && (
-            <SkeletonLoader
-              width={300}
-              height={200}
-              borderRadius={5}
-              backgroundColor={'#f0f0f0'}
-              marginBottom={0}
-              onClick={() => {
-                window.open('https://crissrayes.github.io/vernux', '_blank');
-              }}
-            />
-          )}
-          <img
-            className='project-img'
-            style={{
-              display: imagesLoaded[index] ? 'block' : 'none',
-            }}
-            src={url}
-            alt={`Image ${index + 1}`}
-            onClick={() => {
-              window.open('https://crissrayes.github.io/vernux', '_blank');
-            }}
-          />
-        </div>
-      ))}
-
+      <img
+        className='project-img'
+        src='/img/vernux-web.png'
+        alt='Vernux'
+        onClick={() => {
+          window.open('https://crissrayes.github.io/vernux', '_blank');
+        }}
+      />
       <div>
         <h3>About</h3>
         <p>
