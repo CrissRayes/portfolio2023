67435e63082806809c0ccb13c258b2abbb68bb00
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2024-01-04 23:12:02 -0300

    menu hamburguesa

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,47 +1,77 @@
 import { NavLink, Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export const Projects = () => {
   const linkActivo = ({ isActive }) => (isActive ? 'active' : '');
 
+  useEffect(() => {
+    const nav = document.querySelector('.nav-menu');
+    const toggleNavMenu = () => {
+      nav.classList.toggle('open');
+    };
+
+    const closeNavMenu = (e) => {
+      if (!nav.contains(e.target)) {
+        nav.classList.remove('open');
+      }
+    };
+
+    nav.addEventListener('click', toggleNavMenu);
+    nav.addEventListener('click', closeNavMenu);
+
+    // Limpia los eventos al desmontar el componente
+    return () => {
+      nav.removeEventListener('click', toggleNavMenu);
+      nav.removeEventListener('click', closeNavMenu);
+    };
+  }, []);
+
   return (
     <div className='projects'>
       <h2>Projects</h2>
-      <ul>
-        <li>
-          <NavLink
-            to='/projects'
-            className={linkActivo}
-            end
-          >
-            Web Apps
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to='webs'
-            className={linkActivo}
-            end
-          >
-            Websites
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to='apis'
-            className={linkActivo}
-          >
-            Apis
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to='games'
-            className={linkActivo}
-          >
-            Games & Misc
-          </NavLink>
-        </li>
-      </ul>
+      <nav className='nav-menu'>
+        <div className='hamburger'>
+          <div className='icon'></div>
+          <div className='icon'></div>
+          <div className='icon'></div>
+        </div>
+        <ul>
+          <li>
+            <NavLink
+              to='/projects'
+              className={linkActivo}
+              end
+            >
+              Web Apps
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='webs'
+              className={linkActivo}
+              end
+            >
+              Websites
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='apis'
+              className={linkActivo}
+            >
+              Apis
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='games'
+              className={linkActivo}
+            >
+              Games & Misc
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
       <Outlet />
     </div>
   );
