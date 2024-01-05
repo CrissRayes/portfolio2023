0b7486611539fd79346afc5f956989318ac67dff
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2024-01-04 21:48:47 -0300

    add project

--- src/pages/GameProjects.jsx
+++ src/pages/GameProjects.jsx
@@ -1,7 +1,34 @@
+import { games } from '../data/data';
+
 export const GameProjects = () => {
   return (
-    <div>
-      <p>Listado de juegos desde una api</p>
+    <div className='container-cards'>
+      {games.map((game) => (
+        <div
+          className='card'
+          key={game.id}
+        >
+          <img
+            className='card-img'
+            src={game.img}
+            alt={game.title}
+            onClick={() => {
+              window.open(game.url, '_blank');
+            }}
+          />
+          <div className='card-content'>
+            <h3>{game.title}</h3>
+            {game.technologies.map((technology) => (
+              <span
+                className='pill'
+                key={technology}
+              >
+                {technology}
+              </span>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
