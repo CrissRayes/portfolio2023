0956728d73693d0eda598abeb170527e9399b810
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2024-01-09 15:39:40 -0300

    refactoriza con ProjectCard

--- src/pages/GameProjects.jsx
+++ src/pages/GameProjects.jsx
@@ -1,30 +1,14 @@
+import { ProjectCard } from '../components/ProjectCard';
 import { games } from '../data/data';
 
 export const GameProjects = () => {
   return (
     <div className='container-cards'>
       {games.map((game) => (
-        <div
-          className='card'
+        <ProjectCard
           key={game.id}
-        >
-          <img
-            className='card-img'
-            src={game.img}
-            alt={game.title}
-          />
-          <div className='card-content'>
-            <center>{game.title}</center>
-            <button
-              className='card-btn'
-              onClick={() => {
-                window.open(game.url, '_blank');
-              }}
-            >
-              View Project
-            </button>
-          </div>
-        </div>
+          {...game}
+        />
       ))}
     </div>
   );
